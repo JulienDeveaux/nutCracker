05360fd56f9abf038eb13bfc4c81e6baad5c0d27
Julien Deveaux <julien.deveaux@hotmail.com>
2022-12-13 16:27:30 +0100

    feat: Add alphabet equal splitting method

--- nutCracker/Services/WebsocketService.cs
+++ nutCracker/Services/WebsocketService.cs
@@ -7,17 +7,13 @@ namespace nutCracker.Services;
 public class WebsocketService
 {
     public const int MaxPasswordLength = 4;
-    // [a-zA-Z0-9]                  //min/ maj/ nb
-    public static readonly char[] CharactersAvailable = {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    };
-    
+
+    public static string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
     private static int _totalSlavesCount = 0;
-    
+
     private List<Slave> Slaves { get; }
-    
+
     /// <summary>
     ///  null value = pending <br/>
     ///  empty value = not found <br/>
@@ -30,7 +26,7 @@ public class WebsocketService
         Slaves = new List<Slave>();
         Md5Hashes = new Dictionary<string, string>();
     }
-    
+
     public async Task RegisterSlave(WebSocket socket)
     {
         var slave = new Slave
@@ -39,9 +35,9 @@ public class WebsocketService
             WebSocket = socket,
             Status = SlaveStatus.Ready
         };
-        
+
         Slaves.Add(slave);
-        
+
         var buffer = new byte[4 * 1024];
 
         WebSocketReceiveResult received;
@@ -51,25 +47,24 @@ public class WebsocketService
             received = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
 
             var message = Encoding.UTF8.GetString(buffer, 0, received.Count);
-            
+
             Console.WriteLine($"message received from slave {slave.Id}: '{message}'");
 
             var tab = message.Split(' ');
-            
+
             switch (tab[0])
             {
                 case "found" when tab.Length == 3:
                     Md5Hashes[tab[1]] = tab[2];
                     slave.Status = SlaveStatus.Ready;
-                break;
-                
+                    break;
+
                 case "notFound" when tab.Length == 3:
                     Md5Hashes[tab[1]] = string.Empty;
                     slave.Status = SlaveStatus.Ready;
-                break;
+                    break;
             }
-        } 
-        while (!received.CloseStatus.HasValue);
+        } while (!received.CloseStatus.HasValue);
 
         slave.Status = SlaveStatus.Dead;
 
@@ -77,7 +72,7 @@ public class WebsocketService
 
         Slaves.Remove(slave);
     }
-    
+
     public async Task<string> Crack(string md5Hash)
     {
         var waitingSlaves = Slaves.Where(s => s.Status == SlaveStatus.Ready).ToArray();
@@ -86,25 +81,26 @@ public class WebsocketService
         {
             return null;
         }
-        
+
         var allAlphabets = DetermineAlphabets(waitingSlaves.Length);
-        
+
         Md5Hashes[md5Hash] = null;
 
-        for(var i = 0; i < waitingSlaves.Length; i++)
+        for (var i = 0; i < waitingSlaves.Length; i++)
         {
             var slave = waitingSlaves[i];
             var alphabet = allAlphabets[i];
-            
+
             slave.Status = SlaveStatus.Working;
 
             await slave.WebSocket.SendAsync(
-                new ArraySegment<byte>(Encoding.UTF8.GetBytes($"search {md5Hash} {alphabet[..alphabet.IndexOf("|", StringComparison.Ordinal)]} {alphabet[(alphabet.IndexOf("|", StringComparison.Ordinal) + 1)..]}")),
-                WebSocketMessageType.Text, 
-                true, 
+                new ArraySegment<byte>(Encoding.UTF8.GetBytes(
+                    $"search {md5Hash} {alphabet[..alphabet.IndexOf("|", StringComparison.Ordinal)]} {alphabet[(alphabet.IndexOf("|", StringComparison.Ordinal) + 1)..]}")),
+                WebSocketMessageType.Text,
+                true,
                 CancellationToken.None);
         }
-        
+
         while (string.IsNullOrWhiteSpace(Md5Hashes[md5Hash]) && waitingSlaves.Any(s => s.Status == SlaveStatus.Working))
         {
             await Task.Delay(100);
@@ -115,18 +111,99 @@ public class WebsocketService
 
     private string[] DetermineAlphabets(int nbSlaves)
     {
-        var alphabets = new string[nbSlaves];
-        
-        var nb = MaxPasswordLength / nbSlaves;
-
-        // todo split alphabet in nbSlaves parts
-        for (var i = 0; i < nbSlaves; i++)
+        var alphabetsToReturn = new string[nbSlaves];
+        alphabetsToReturn[0] = "a";
+        long alphabetSize;
+        if (MaxPasswordLength == 1)
         {
-            var nextNb = nb * (i + 1);
-
-            alphabets[i] = $"{CharactersAvailable[nb * i]}|{(string.Join("", new char[MaxPasswordLength].Select(_ => CharactersAvailable[Math.Min(nextNb, CharactersAvailable.Length - 1)])))}";
+            alphabetSize = (long)Math.Round(Math.Pow(62, MaxPasswordLength));
         }
-        
-        return alphabets;
+        else
+        {
+            alphabetSize = (long)Math.Round(Math.Pow(62, MaxPasswordLength + 1));
+        }
+
+        var split_position = (int)Math.Round((1.0 * alphabetSize / nbSlaves));
+        var split_positions = new int[nbSlaves - 1];
+        var it = split_position;
+        for (var i = 0; i < split_positions.Length; i++)
+        {
+            split_positions[i] = it;
+            it += split_position;
+        }
+
+        var previousEnd = "a";
+        for (var i = 0; i < split_positions.Length; i++)
+        {
+            var transformIn64Format = new int[MaxPasswordLength];
+            for (var j = 0; j < MaxPasswordLength; j++)
+            {
+                var wordPos = (1.0 * split_positions[i] / (62 * MaxPasswordLength)) * 62;
+                var letterPos = (int)Math.Round(((wordPos / MaxPasswordLength) * (j + 1)));
+                var toLetter = letterPos % 62;
+                transformIn64Format[j] = toLetter;
+            }
+
+            var nextWord = "";
+            foreach (var letter in transformIn64Format)
+            {
+                nextWord += alphabet[letter];
+            }
+
+            alphabetsToReturn[i] = previousEnd + "|" + nextWord;
+            previousEnd = "";
+
+            var letterArray = new int[MaxPasswordLength];
+            for (var j = 0; j < transformIn64Format.Length; j++)
+            {
+                var num = transformIn64Format[j];
+                if (num >= 62)
+                {
+                    num = 61;
+                }
+
+                letterArray[j] = num;
+
+                if (j == transformIn64Format.Length - 1)
+                {
+                    if (letterArray[j] == 61)
+                    {
+                        letterArray[j] = 0;
+                        var depth = 1;
+                        while (depth < MaxPasswordLength)
+                        {
+                            if (letterArray[j - depth] < 61)
+                            {
+                                letterArray[j - depth] += 1;
+                                depth = MaxPasswordLength;
+                            }
+                            else
+                            {
+                                letterArray[j - depth] = 0;
+                                depth++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        letterArray[j] += 1;
+                    }
+                }
+            }
+
+            foreach (var letter in letterArray)
+            {
+                previousEnd += alphabet[letter];
+            }
+        }
+
+        var howMany9 = "";
+        for (var i = 0; i < MaxPasswordLength; i++)
+        {
+            howMany9 += "9";
+        }
+
+        alphabetsToReturn[nbSlaves - 1] = previousEnd + "|" + howMany9;
+        return alphabetsToReturn;
     }
 }
