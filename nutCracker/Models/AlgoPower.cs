df748b6b221a5b52afc668b453825c17a90bd6d6
Dim145 <dimitri.bleach@gmail.com>
2022-12-19 21:02:48 +0100

    feat: start to add power (nb of slave) => front only

---
+++ nutCracker/Models/AlgoPower.cs
@@ -0,0 +1,19 @@
+﻿namespace nutCracker.Models;
+
+public enum AlgoPower: byte
+{
+    Classic = 1,
+    More = 2,
+    Fast = 4,
+    Fastest = 8,
+    FastestPlus = 16,
+    Extreme = 32,
+    ExtremePlus = 64,
+    Haaaaaaaaaaaaaaaaaaaaaaaaa = 128,
+    IamDead = byte.MaxValue
+}
+
+public static class AlgoPowerExtension
+{
+    public static byte Value(this AlgoPower power) => (byte) power;
+}
